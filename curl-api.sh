05e6500be1de73207766c3ebae5ced0e96ad4161
Ritesh Pradhan <pradhan@rcorp.co.in>
2019-07-20 12:57:23 +0530

    Create curl-api.sh

---
+++ curl-api.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s -o /dev/null -w "%{http_code}\n" https://www.google.com
+curl -s -o /dev/null -w "%{http_code}\n" https://www.google.com
+curl -s -o /dev/null -w "%{http_code}\n" https://www.google.com/jlkl
