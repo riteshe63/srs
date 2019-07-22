435eafbf0d75edbc4c40d9c894c6dd4dc39fec4b
Ritesh Pradhan <pradhan@rcorp.co.in>
2019-07-22 11:28:01 +0530

    Final update sh file

--- curl.sh
+++ curl.sh
@@ -4,8 +4,8 @@ sh curl-api.sh  > tt.txt
 awk '!/200/' tt.txt > tt1.txt
 if [ -s ./tt1.txt ]
 then
-     echo "File not empty"
+     echo "File Not Empty"
+     currentBuild.result = 'ABORTED';
 else
      echo "File empty"
-     cp ./tt1.txtt /jk/k
 fi
